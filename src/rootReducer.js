b3aa6195aed0cabcd2fc83184d685cd56d14640c
von Buelow <jaspar@UMICH.EDU>
2020-08-06 02:47:53 -0400

    Build add and remove cases

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,25 @@
-const INITIAL_STATE = {items: []};
+import data from './data.json';
+import { ADD_ITEM, REMOVE_ITEM } from "./actionTypes";
+
+const INITIAL_STATE = {products: data.products, cart: {}};
 
 function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case "ADD_ITEM":
-      return { ...state, items: [ ...state.items, { ...action.item }] };
+    case ADD_ITEM:
+      const cartCopy = { ...state.cart };
+      cartCopy[action.id] = (cartCopy[action.id] || 0) + 1;
+      return { ...state, cart: cartCopy };
 
-    case "DELETE_ITEM":
-      return { ...state, items: state.items.filter(item => item.id !== action.id)};
+    case REMOVE_ITEM:
+      const cartCopy = { ...state.cart };
+      if (!cartCopy[action.id]) {
+        return state;
+      }
+      cartCopy[action.id]--;
+      if(!cartCopy[action.id]) {
+        delete cartCopy[action.id];
+      }
+      return { ...state, cart: cartCopy };
 
     default:
       return state;
