b39033ab7af4949b2d28119829ad56ce20ad30de
von Buelow <jaspar@UMICH.EDU>
2020-08-06 21:03:35 -0400

    Routes component

--- src/ProductButtons.js
+++ src/ProductButtons.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addItem, removeItem } from './actions';
+import './ProductButtons.css'
 
 function ProductButtons({id}) {
   const dispatch = useDispatch();
 
-  function removeCartItem() {
+  const removeCartItem = () => {
     dispatch(removeItem(id));
   }
 
-  function addCartItem() {
+  const addCartItem = () => {
     dispatch(addItem(id));
   };
   
@@ -17,11 +18,11 @@ function ProductButtons({id}) {
     <div className="d-flex justify-content-between">
       <i
         onClick={addCartItem}
-        className="CartIcon fas fa-cart-plus fa-2x text-success"
+        className="ProductButtons fas fa-cart-plus fa-2x text-success"
       />
       <i
         onClick={removeCartItem}
-        className="CartIcon fas fa-cart-arrow-down fa-2x text-danger"
+        className="ProductButtons fas fa-cart-arrow-down fa-2x text-danger"
       />
     </div>
   );
