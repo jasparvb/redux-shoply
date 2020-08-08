dedee65966b7f8849585474e3e9f841fd72ac6c4
von Buelow <jaspar@UMICH.EDU>
2020-08-08 11:46:10 -0400

    Cart total helper fn

---
+++ src/helpers.js
@@ -0,0 +1,10 @@
+export function cartTotal(products, cart) {
+    let total = 0;
+    for (let id in products) {
+      const { price } = products[id];
+      const quantity = cart[id] || 0;
+      total += price * quantity;
+    }
+    return total;
+}
+  
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,14 +1,15 @@
 import data from './data.json';
 import { ADD_ITEM, REMOVE_ITEM } from "./actionTypes";
+import { cartTotal } from './helpers';
 
-const INITIAL_STATE = {products: data.products, cart: {}};
+const INITIAL_STATE = {products: data.products, cart: {}, cartTotal: 0};
 
 function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD_ITEM: {
       const cartCopy = { ...state.cart };
       cartCopy[action.id] = (cartCopy[action.id] || 0) + 1;
-      return { ...state, cart: cartCopy };
+      return { ...state, cart: cartCopy, cartTotal: cartTotal(state.products, cartCopy)};
     }
       
     case REMOVE_ITEM: {
@@ -20,7 +21,7 @@ function rootReducer(state = INITIAL_STATE, action) {
       if(!cartCopy[action.id]) {
         delete cartCopy[action.id];
       }
-      return { ...state, cart: cartCopy };
+      return { ...state, cart: cartCopy, cartTotal: cartTotal(state.products, cartCopy)};
     }
 
     default:
